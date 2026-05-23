df47e11e425ef067598db6b19956f196bfe4085b
Robert Kelly <robert.kelly@3vuellc.com>
2026-05-22 23:03:25 -0500

    get resume content added

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,7 +1,156 @@
 import React from "react";
 
 const Resume = () => {
-	return <div>Resume</div>;
+	return (
+		<div className="resume">
+			<h2 className="resume__heading">EXPERIENCE</h2>
+
+			<div className="resume__item resume__item--fcb-chicago">
+				<div className="resume__company">FCB Chicago //</div>
+				<div className="resume__role">Associate Creative Director</div>
+				<div className="resume__date">Oct 2019 - Jan 2026</div>
+
+				<div className="resume__clients">
+					Clients: Harley-Davidson, Cox Communications, City of
+					Chicago, Volkswagen
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__work">
+					The work: Creative team lead across digital campaigns, web &
+					UX development, TV & video, social media and brand
+					activations
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__win">
+					Big win: Spearheaded creative and strategy for Chicago's 'We
+					Are All One Home Team' health campaign, quickly developing
+					multi-channel content (digital, social, OOH, TV, PR) that
+					united and informed millions of city residents during a
+					pivotal public health period.
+				</div>
+			</div>
+
+			<div className="resume__item resume__item--rise-interactive">
+				<div className="resume__company">Rise Interactive //</div>
+				<div className="resume__role">
+					Sr Copywriter & Content Strategist
+				</div>
+				<div className="resume__date">Jan 2017 - Oct 2019</div>
+
+				<div className="resume__clients">
+					Clients: Converse, Michaels Crafts, National Dairy Council,
+					Nicklaus Children’s Hospital, Tempur-Sealy, Atkins,
+					Nordstrom, U.S. Foods, SKIL Power Tools
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__work">
+					The work: Digital campaigns, social campaigns, web copy and
+					strategy, UX copy and strategy, editorial and infographics
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__win">
+					Big win: Helped lead the digital creative vision and
+					copywriting for the Michaels Weddings campaign, successfully
+					positioning the brand as a go-to resource and unlocking a
+					new online revenue stream.
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__awards">
+					Awards: 2020 Internet Advertising Competition Winner,
+					NicklausChildrens.org
+				</div>
+			</div>
+
+			<div className="resume__item resume__item--rpm-advertising">
+				<div className="resume__company">RPM Advertising //</div>
+				<div className="resume__role">Sr Copywriter</div>
+				<div className="resume__date">June 2013 - Jan 2017</div>
+
+				<div className="resume__clients">
+					Clients: Thunder Valley Resort, Penn National Gaming,
+					Hollywood Casinos, Terlato Wines, Riondo Prosecco, Gila
+					River Casinos
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__work">
+					The work: TV, radio, outdoor, print ads, digital banners,
+					web copy, social media, direct mail and branding
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__win">
+					Big win: Launched the 'Live Out Loud' brand campaign for
+					Thunder Valley Resort, creating messaging across TV, radio,
+					social and outdoor ads that drove a 60% yearly increase in
+					resort bookings.
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__awards">
+					Awards: 2015 Davey Awards, "Hollywood Casino, All That
+					Glitters"
+				</div>
+			</div>
+
+			<div className="resume__item resume__item--walgreens">
+				<div className="resume__company">Walgreens Co. //</div>
+				<div className="resume__role">Digital Writer & Strategist</div>
+				<div className="resume__date">2012 - 2013</div>
+
+				<div className="resume__clients">
+					Clients: Walgreens private label brands: Well at Walgreens,
+					Ology, Well Beginnings Baby, Nice!, Good & Delish
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__work">
+					The work: Digital creative, blogging, social media
+					campaigns, web copy
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__win">
+					Big win: Developing the digital creative campaign for the
+					All-Star Celebrity Apprentice integration.
+				</div>
+
+				<div className="resume__spacer">​</div>
+
+				<div className="resume__awards">
+					Awards: 2013 Store Brands Decisions Innovation Awards, Best
+					Television Advertising Campaign "Walgreens Good &Delish,
+					Celebrity Apprentice"
+				</div>
+			</div>
+
+			<div className="resume__item resume__item--early">
+				<div className="resume__company">
+					Copywriting from way back //
+				</div>
+
+				<div className="resume__clients">
+					ChiCitySports.com, Second City Training Center, Arc
+					Worldwide, Illini Athletics
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Resume;
